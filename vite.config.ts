d4c29eafc83d73a272d9a0e708c45c290abd01c0
anthonyASC5 <52748939+anthonyASC5@users.noreply.github.com>
2026-01-08 03:59:58 +0000

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/anthonys-project/', // must match repo name exactly (lowercase)
   plugins: [react()],
   resolve: {
     alias: {
